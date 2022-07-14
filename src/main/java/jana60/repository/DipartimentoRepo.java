7e3d2452fdc5240b157ea5177323101f674f6b16
MattiaVerardi <verardi.mattia97@gmail.com>
2022-07-14 18:01:38 +0200

    consegna bonus

---
+++ src/main/java/jana60/repository/DipartimentoRepo.java
@@ -0,0 +1,9 @@
+package jana60.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import jana60.model.Dipartimenti;
+
+public interface DipartimentoRepo extends CrudRepository<Dipartimenti, Integer> {
+
+}
